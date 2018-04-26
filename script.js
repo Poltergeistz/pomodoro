0d43868156f4594bef849e449bdd3a61fbccea5a
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-26 21:40:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+/*
+var liste = new Array();
+
+function register(){
+    var tache = document.getElementById('Text1').value; //Cette fonction ne fonctionne pas. On ne stocke
+    console.log(tache);
+    liste[liste.length] = tache;
+    document.getElementById("todo").innerHTML += "<div id=\"truc" + liste.length + "\">" + tache + "<button onclick=\"erase ()\">Erase</button>" + "</div>";
+}
+*/
+function todoElement () {
+    var tache = document.getElementById('text1').value;
+
+    document.write()
+    console.log(tache)
+}
+
+function erase () {
+    
+    document.getElementById("truc1").style.color = red;/*Changement de couleur*/;
+
+}
+
+
+
